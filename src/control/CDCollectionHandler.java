4961864f867c417e82ba0a041f7d9c551712870e
moushaya <hayamou2006@yahoo.com>
2023-09-18 09:27:06 +0200

    feat(src/CDCollectionHandler): Finished TODOs till 5

--- src/control/CDCollectionHandler.java
+++ src/control/CDCollectionHandler.java
@@ -17,7 +17,10 @@ public class CDCollectionHandler {
      */
     public CDCollectionHandler(int[] amounts){
         //TODO: 01 - Konstruktor fertigstellen.
-
+        allCDs = new CompactDisc[4][];
+        for (int i = 0; i < allCDs.length; i++) {
+            allCDs[i] = new CompactDisc[amounts[i]];
+        }
     }
 
     /**
@@ -31,6 +34,10 @@ public class CDCollectionHandler {
      */
     public boolean addNewCD(int box, int place, String artist, String title){
         //TODO: 02 - Hinzufügen einer CD
+        if (allCDs.length > box && allCDs[box].length > place && allCDs[box][place] == null) {
+            allCDs[box][place] = new CompactDisc(artist, title);
+            return true;
+        }
         return false;
     }
 
@@ -43,6 +50,13 @@ public class CDCollectionHandler {
      */
     public String[] getInfo(int box, int place){
         String[] output = new String[2];
+        if (allCDs.length > box && allCDs[box].length > place && allCDs[box][place] != null) {
+            output[0] = allCDs[box][place].getArtist();
+            output[1] = allCDs[box][place].getTitle();
+        } else {
+            output[0] = "Empty";
+            output[1] = "Empty";
+        }
         //TODO: 03 - Informationen zu einer bestimmen CD
         return output;
     }
@@ -56,6 +70,10 @@ public class CDCollectionHandler {
      */
     public boolean releaseCD(int box, int place){
         //TODO: 04 - Entfernen einer bestimmten CD
+        if (allCDs.length > box && allCDs[box].length > place && allCDs[box][place] != null) {
+            allCDs[box][place] = null;
+            return true;
+        }
         return false;
     }
 
@@ -67,6 +85,11 @@ public class CDCollectionHandler {
      */
     public String[] getAllCDsFrom(int box){
         //TODO: 05 - Vollständige Informationsausgabe aller CDs - Nach Fertigstellung im MainPanelHandler Zeile 165-167 entkommentieren
+        if (box > allCDs.length) {
+            /*String[] array = new String[allCDs[box].length*2];
+            for ()
+            return array[];*/
+        }
         return null;
     }
 
